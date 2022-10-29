f4478fc4d4ced52c52e7a254b75184e7a77b0243
relatora <relatora@gmail.com>
2022-10-29 15:29:32 -0400

    first commit

---
+++ next.confg.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: [
+      'links.papareact.com',
+      'fakestoreapi.com',
+    ],
+    // formats: ["image/avif", "image/webp"],
+  },
+};
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Header from './../components/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
+
+      {/* ---- Header ---- */}
+        <Header/>
+    </div>
+  );
+}
